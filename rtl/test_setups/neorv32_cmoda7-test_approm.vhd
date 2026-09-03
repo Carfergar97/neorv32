cdb0c37383589c3b157f1a85a23ab0c6c27a38a4
Carfergar97 <cfdezg1997@gmail.com>
2026-09-03 15:01:10 +0200

    chore: cmod-a7 test RTL top level initial commit

---
+++ rtl/test_setups/neorv32_cmoda7-test_approm.vhd
@@ -0,0 +1,75 @@
+-- ================================================================================ --
+-- NEORV32 - Test Setup Using The Internal IMEM To Run Pre-Installed Executables    --
+-- -------------------------------------------------------------------------------- --
+-- The NEORV32 RISC-V Processor - https://github.com/stnolting/neorv32              --
+-- Copyright (c) NEORV32 contributors.                                              --
+-- Copyright (c) 2020 - 2026 Stephan Nolting. All rights reserved.                  --
+-- Licensed under the BSD-3-Clause license, see LICENSE for details.                --
+-- SPDX-License-Identifier: BSD-3-Clause                                            --
+-- ================================================================================ --
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library neorv32;
+use neorv32.neorv32_package.all;
+
+entity neorv32_cmoda7_test_setup_approm is
+  generic (
+    -- adapt these for your setup --
+    CLOCK_FREQUENCY : natural := 12000000; -- clock frequency of clk_i in Hz
+    IMEM_SIZE       : natural := 16*1024;   -- size of processor-internal instruction memory in bytes
+    DMEM_SIZE       : natural := 8*1024     -- size of processor-internal data memory in bytes
+  );
+  port (
+    -- Global control --
+    clk_i  : in  std_ulogic; -- global clock, rising edge
+    rstn_i : in  std_ulogic; -- global reset, low-active, async
+    -- GPIO --
+    uart0_txd_o : out std_ulogic; -- UART0 send data
+    uart0_rxd_i : in  std_ulogic  -- UART0 receive data
+  );
+end entity;
+
+architecture neorv32_cmoda7_test_setup_approm_rtl of neorv32_cmoda7_test_setup_approm is
+
+  signal w_rstn: std_ulogic;
+
+begin
+
+  -- The Core Of The Problem ----------------------------------------------------------------
+  -- -------------------------------------------------------------------------------------------
+  neorv32_top_inst: neorv32_top
+  generic map (
+    -- Clocking --
+    CLOCK_FREQUENCY  => CLOCK_FREQUENCY, -- clock frequency of clk_i in Hz
+    -- Boot Configuration --
+    BOOT_MODE_SELECT => 2,               -- boot from pre-initialized IMEM
+    -- RISC-V CPU Extensions --
+    RISCV_ISA_C      => true,            -- implement compressed extension?
+    RISCV_ISA_M      => true,            -- implement mul/div extension?
+    RISCV_ISA_Zicntr => true,            -- implement base counters?
+    -- Internal Instruction memory --
+    IMEM_EN          => true,            -- implement processor-internal instruction memory
+    IMEM_SIZE        => IMEM_SIZE,       -- size of processor-internal instruction memory in bytes
+    -- Internal Data memory --
+    DMEM_EN          => true,            -- implement processor-internal data memory
+    DMEM_SIZE        => DMEM_SIZE,       -- size of processor-internal data memory in bytes
+    -- Processor peripherals --
+    IO_CLINT_EN      => true,             -- implement core local interruptor (CLINT)?
+    IO_UART0_EN => true
+  )
+  port map (
+    -- Global control --
+    clk_i  => clk_i,       -- global clock, rising edge
+    rstn_i => w_rstn,      -- global reset, low-active, async
+    -- GPIO (available if IO_GPIO_NUM > 0) --
+    uart0_txd_o => uart0_txd_o,  -- UART0 send data
+    uart0_rxd_i => uart0_rxd_i   -- UART0 receive data
+  );
+
+  -- RST logic for the cmoda7 board --
+  w_rstn <= not rstn_i;
+
+end architecture;
